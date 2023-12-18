3014f0c738cead21df13ca384d2434f23228e68a
guuhmoraess <152665401+guuhmoraess@users.noreply.github.com>
2023-12-18 20:38:46 -0300

    attttt

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require ('express')
 const connectDatabase = require ('./dataBase')
 const rotas = require ('./rotas')
 const app = express ()
-const port = 4000
+const port = 5000
 app.use (express.json())
 connectDatabase ()
 app.use(rotas)
