7fe39573dc8e388e75b2b7ab236b6d6943d369fe
guuhmoraess <152665401+guuhmoraess@users.noreply.github.com>
2023-12-18 20:15:43 -0300

    att port

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require ('express')
 const connectDatabase = require ('./dataBase')
 const rotas = require ('./rotas')
 const app = express ()
-const port = 3005
+const port = 3006
 app.use (express.json())
 connectDatabase ()
 app.use(rotas)
