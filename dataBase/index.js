2766543552a5ead67287d1488c2a2b56bfd250b9
guuhmoraess <152665401+guuhmoraess@users.noreply.github.com>
2023-12-19 21:33:09 -0300

    att banco

--- dataBase/index.js
+++ dataBase/index.js
@@ -1,16 +1,22 @@
 const mongoose = require('mongoose');
 
-const connectDatabase = () => {
+const connectDatabase = async () => {
+    try {
+        console.log('Espere estamos conectando');
 
-    console.log("Espere estamos conectando")
+        await mongoose.connect("mongodb+srv://gustavo:eGKGOEPT4BOepzbQ@cluster0.1duey0r.mongodb.net/?retryWrites=true&w=majority", {
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        });
 
-    mongoose.connect("mongodb+srv://gustavo:eGKGOEPT4BOepzbQ@cluster0.1duey0r.mongodb.net/?retryWrites=true&w=majority", { useNewurlparser: true, useUnifiedTopology: true })
+        console.log('Banco conectado');
+        // Inicialize o servidor aqui
+        app.listen(3008, () => {
+            console.log('Servidor rodando na porta 3008');
+        });
+    } catch (error) {
+        console.error('Erro ao conectar ao MongoDB:', error);
+    }
+};
 
-        .then(() => console.log("Banco conectado"))
-
-
-
-        .catch((error) => console.log(error))
-
-}
-module.exports = connectDatabase
+module.exports = connectDatabase;
